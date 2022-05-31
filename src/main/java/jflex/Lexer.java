49502adc0a309f94b2143496436064f132d96da1
chrysitis <amchrisag@gmail.com>
2022-05-30 23:31:43 -0600

    Float operations added. :snowflake:

--- src/main/java/jflex/Lexer.java
+++ src/main/java/jflex/Lexer.java
@@ -1077,17 +1077,17 @@ public class Lexer implements java_cup.runtime.Scanner {
                       if(isFunction) {
                         currentSymbolTable.addFuncParams(idType, yytext());
                         isFunction = false;
-                        System.out.println("AGREGANDO PARAMETRO: " + idType + " " + yytext());
+                        //System.out.println("AGREGANDO PARAMETRO: " + idType + " " + yytext());
                       } else {
                         tokenAttributes.add(idType);
-                        System.out.println("AGREGANDO VARIABLE: " + idType + " " + yytext());
+                        //System.out.println("AGREGANDO VARIABLE: " + idType + " " + yytext());
                         currentSymbolTable.addSymbol(yytext(), tokenAttributes);  
                       }
                       this.idType = "";
                       return symbol(sym.ID, yytext());  
                     } else {
                       if (isReturnVal) {
-                        System.out.println("RETURN VALUE IS: " + yytext()); 
+                        //System.out.println("RETURN VALUE IS: " + yytext()); 
                         currentSymbolTable.setReturnVal(yytext());
                         isReturnVal = false;
                       }
@@ -1176,7 +1176,7 @@ public class Lexer implements java_cup.runtime.Scanner {
           case 32:
             { Boolean func = (idType != "");
                   if (func) {
-                    System.out.println("TABLE FOR: " + yytext() + " WITH SCOPE: " + this.scope);
+                    //System.out.println("TABLE FOR: " + yytext() + " WITH SCOPE: " + this.scope);
                     createSymbolTable(false, yytext(), idType);
                     this.isFunction = true; 
                   } 
@@ -1230,6 +1230,7 @@ public class Lexer implements java_cup.runtime.Scanner {
             { saveToken(sym.INT, yytext());
                   idType = "int";
                   this.idTypeExamination = "int";
+                  //System.out.println("TYPE EXAMINED IS: " + idTypeExamination);
                   return symbol(sym.INT, yytext());
             }
             // fall through
